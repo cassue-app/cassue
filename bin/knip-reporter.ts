635eb712e6a2123da9e939d029ae5fbb07324ca6
S. Suzuki <terrierscript@gmail.com>
2025-02-14 22:13:49 +0900

    add

--- bin/knip-reporter.ts
+++ bin/knip-reporter.ts
@@ -1,5 +1,6 @@
 
 import type { Reporter } from 'knip'
+import { IssueRecords } from 'knip/dist/types/issues'
 
 // https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions#setting-a-warning-message
 export const ISSUE_TYPE_TITLE: Record<string, string> = {
@@ -24,18 +25,39 @@ const parseFileIssue = (value: Set<string>) => {
   const message = ISSUE_TYPE_TITLE["files"]
   return Array.from(value.values()).map(file => {
     return `::warning file=${file},title=${message}`
-  }).join("\n")
-
+  })
 }
+const parseIssueRecords = (type: string, value: IssueRecords) => {
+  const message = ISSUE_TYPE_TITLE[type]
+  return Object.entries(value).map(([file, v]) => {
+    return Object.entries(v).map(([target, value]) => {
+      if (value.symbols) {
+        return value.symbols.map(symbol => {
+          return `::warning file=${file},title=${message},line=${symbol.line},col=${symbol.col}::${symbol.symbol}`
+        })
+      }
+      if (value.col) {
+        return `::warning file=${file},title=${message},line=${value.line},col=${value.col}::${target}`
+      }
+    })
+    // console.log(message, { k, v })
+    // return `::warning file=${file},title=${message}`
+  })
+}
+
 const reporter: Reporter = function (options) {
 
+  console.log(options.issues.files)
   const record = Object.entries(options.issues).map(([key, value]) => {
     switch (key) {
       case "files":
         return parseFileIssue(value as Set<string>)
+      default:
+        return parseIssueRecords(key, value as IssueRecords)
     }
     // console.log({ key, value })
-  })
+  }).flat(10).filter(record => record && record?.length > 0)
+
   console.log(record.join("\n"))
   // options.issues.files
 
